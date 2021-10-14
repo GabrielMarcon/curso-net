416a6e8283a31d4b6f7785ae6f4140e5b76da134
Gabriel Marcon <gabriel.marcon2009@gmail.com>
2021-10-14 20:05:16 -0300

    Primeiro commit

---
+++ ForAll/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace ForAll
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
